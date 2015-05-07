60af0bb40c8c340fe51e4531cea2a2a74f145d0c
Kaz Nishimura <kazssym@vx68k.org>
2015-05-07 12:41:11 +0900

    Added initial files.

---
+++ hgext/convertprcs/__init__.py
@@ -0,0 +1,20 @@
+# __init__.py for convertprcs
+# Copyright (C) 2015 Kaz Nishimura
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from hgext.convert.convcmd import source_converters
+from prcs import prcs_source
+
+source_converters.append(('prcs', prcs_source, 'branchsort'))
---
+++ hgext/convertprcs/prcs.py
@@ -0,0 +1,33 @@
+# prcs.py for convertprcs
+# Copyright (C) 2015 Kaz Nishimura
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from hgext.convert.common import converter_source
+
+class prcs_source(converter_source):
+    """ """
+
+    def __init__(self, ui, path, rev):
+        """ """
+
+    def getheads(self):
+        return []
+
+    def getchanges(self):
+        return []
+
+    def gettags(self):
+        """Return an empty dictionary since PRCS has no tags."""
+        return {}
