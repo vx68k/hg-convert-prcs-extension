86253850bce998c1411a7b752ca8cce363d6d773
Kaz Nishimura <kazssym@vx68k.org>
2019-04-19 12:55:42 +0900

    Update the package requirements

--- setup.py
+++ setup.py
@@ -57,8 +57,11 @@ if __name__ == "__main__":
             "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
             "Programming Language :: Python :: 3",
         ],
-        requires=["prcslib"],
         python_requires=">= 3",
+        install_requires=[
+            "mercurial",
+            "prcslib"
+        ],
 
         packages=find_packages(exclude=["testsuite", "testsuite.*"]),
 #        test_suite="testsuite",
