6ffefa8bca8704b7cfc8eb7c1cf732cc3171c9cc
Kaz Nishimura <kazssym@linuxfront.com>
2020-03-24 13:37:28 +0900

    Update dependencies

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
         python_requires=">=2.7",
         install_requires=[
             "mercurial>=4.7",
-            "prcslib>=1.0b4"
+            "prcslib>=1.0"
         ],
 
         packages=[
